90fdace9404f9cb4cf3f8bfcacce75b038824c3b
Ma'shum Abdul Jabbar <multimediary@gmail.com>
2022-09-04 00:42:08 +0700

    Update

---
+++ Day07-Problem Solving Paradigm/Tugas/Tugas/src/BruteForce.java
@@ -0,0 +1,22 @@
+
+public class BruteForce {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(cek(6, 6, 14));
+	}
+
+	private static int cek(int A, int B, int C) {
+			
+			int x, y,z;
+			
+			A = x + y + z;
+			B = x * y * z;
+			C = (int) (Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+			
+			return A;
+		
+	}
+	
+	
+
+}
